b03c021bebd0f1beaa163f672862b96047ffa6ad
samettopcu88 <samet.topcu.88@icloud.com>
2024-12-05 07:05:20 +0300

    Add project files.

---
+++ HydrostaticAnalyzer/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace HydrostaticAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(713, 415);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadData.TabIndex = 0;
+            this.btnLoadData.Text = "Yükle";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(676, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLoadData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ HydrostaticAnalyzer/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydrostaticAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        // Tüm hidrostatikler
+        public static double[,] HydrostaticsAll { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // "Veriyi Yükle" butonu tıklama olayı
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialog nesnesi oluştur
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                openFileDialog.Title = "Txt Dosyasını Seç";
+
+                // Kullanıcı dosya seçerse işlemi başlat
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName; // Seçilen dosya yolu
+
+                    try
+                    {
+                        // Verileri txt dosyasından oku
+                        var allData = ReadDataFromTxt(filePath);
+
+                        // Veriyi matrise dönüştür
+                        HydrostaticsAll = ConvertToMatrix(allData);
+
+                        // DataGridView'e veriyi yükle
+                        LoadDataToGridView(HydrostaticsAll);
+
+                        MessageBox.Show("Veriler başarıyla yüklendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        // Txt dosyasını okuma
+        public List<List<double>> ReadDataFromTxt(string filePath)
+        {
+            var data = new List<List<double>>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(line)) continue;
+
+                    var values = new List<double>();
+                    foreach (var value in line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (double.TryParse(value, out double result))
+                        {
+                            values.Add(result);
+                        }
+                    }
+
+
+                    if (values.Count == 17) // Her satır 17 değer içermeli
+                        data.Add(values);
+                }
+            }
+
+            return data;
+        }
+
+        // Listeyi matrise çevirme
+        public double[,] ConvertToMatrix(List<List<double>> listData)
+        {
+            int rows = listData.Count;
+            int columns = listData[0].Count;
+            double[,] matrix = new double[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = listData[i][j];
+                }
+            }
+
+            return matrix;
+        }
+
+        // Veriyi DataGridView'e yükleme
+        public void LoadDataToGridView(double[,] matrix)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Sütunlar ekleniyor
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                dataGridView1.Columns.Add($"Column{j + 1}", $"Column {j + 1}");
+            }
+
+            // Satırlar ekleniyor
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = new List<string>();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row.Add(matrix[i, j].ToString());
+                }
+                dataGridView1.Rows.Add(row.ToArray());
+            }
+        }
+    }
+}
